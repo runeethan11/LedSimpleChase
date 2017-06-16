99140fbc90cd3c19d888104376c903069986a07e
Ethan Vlietstra <funethan@hotmail.com>
2017-06-16 13:20:35 -0600

    Here is the code

---
+++ LedSimpleChase.ino
@@ -0,0 +1,38 @@
+// Simple NeoPixel test.  Lights just a few pixels at a time so a
+// 1m strip can safely be powered from Arduino 5V pin.  Arduino
+// may nonetheless hiccup when LEDs are first connected and not
+// accept code.  So upload code first, unplug USB, connect pixels
+// to GND FIRST, then +5V and digital pin 6, then re-plug USB.
+// A working strip will show a few pixels moving down the line,
+// cycling between red, green and blue.  If you get no response,
+// might be connected to wrong end of strip (the end wires, if
+// any, are no indication -- look instead for the data direction
+// arrows printed on the strip).
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN      0
+#define N_LEDS 20
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(20);
+  strip.show();
+}
+
+void loop() {
+ chase(strip.Color(255, 0, 120));
+ chase(strip.Color(0, 200, 180));
+ chase(strip.Color(255, 45, 0));
+}
+
+static void chase(uint32_t c) {
+  for(uint16_t i=0; i<strip.numPixels()+15; i++) {
+      strip.setPixelColor(i  , c); // Draw new pixel
+      strip.setPixelColor(i-15, 0); // Erase pixel a few steps back
+      strip.show();
+      delay(20);
+  }
+}
